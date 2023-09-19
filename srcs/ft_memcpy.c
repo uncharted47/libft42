b2852b2bb3e2129c9a2eb55288828e02ba6dc182
uncharted47 <elmehdi.lyzouli@usmba.ac.ma>
2023-09-19 16:28:12 +0100

    test

--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: elyzouli <elyzouli@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 00:06:23 by elyzouli          #+#    #+#             */
-/*   Updated: 2023/09/19 14:14:19 by elyzouli         ###   ########.fr       */
+/*   Updated: 2023/09/19 16:22:38 by elyzouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	const char	*src_ptr;
 
 	i = 0;
-	dst_ptr = dst;
-	src_ptr = src;
+	dst_ptr = (char *)dst;
+	src_ptr = (const char *)src;
 	while (i < n)
 	{
 		dst_ptr[i] = src_ptr[i];
